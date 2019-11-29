fcd392306008c69e2a9dc13ce95eee1e0aa6c7f6
hecaigui <982791027@qq.com>
2019-11-29 23:25:11 +0800

    fix:1.跨域笔记完善2.axios笔记3.js前端存储之cookie笔记

--- 跨域/CorsFilter.java
+++ 跨域/CorsFilter.java
@@ -7,6 +7,7 @@ package ...base.spring.filter;
 import org.springframework.http.HttpHeaders;
 
 import javax.servlet.*;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,7 +24,6 @@ import java.util.List;
 
 public class CorsFilter implements Filter {
     //private static final List<String> ALLOW_ORIGINS = Config.getListString("allowOrigins", ",");
-    //private static final List<String> ALLOW_ORIGINS = new ArrayList<>(Arrays.asList("*"));
     private static final String REQUEST_OPTIONS = "OPTIONS";
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -33,22 +33,25 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        Enumeration<String> stringEnumeration = request.getHeaderNames();
-       while (stringEnumeration.hasMoreElements()){
-           stringEnumeration.nextElement();
-       }
-        String orgHeader = request.getHeader(HttpHeaders.ORIGIN);
+        // 获取源站
+        String origin  = request.getHeader(HttpHeaders.ORIGIN);
         //if (orgHeader != null && ALLOW_ORIGINS.contains(orgHeader)) {
             // 允许的跨域的域名
-            response.addHeader("Access-Control-Allow-Origin", "*");
+           response.addHeader("Access-Control-Allow-Origin", origin);
             // 允许携带 cookies 等认证信息
             response.addHeader("Access-Control-Allow-Credentials", "true");
+            // 设置cookie
+            //response.addHeader("Set-Cookie","authorization-token=111");
+            //Cookie cookie = new Cookie("authorization-token2","222");
+            //cookie.setDomain(origin);
+            //response.addCookie(cookie);
             // 允许跨域的方法
             response.addHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT, HEAD");
             // 允许跨域请求携带的请求头
-            response.addHeader("Access-Control-Allow-Headers", "Content-Type, Content-Length, Authorization, Accept, X-Requested-With");
+            response.addHeader("Access-Control-Allow-Headers", "Content-Type, Content-Length, auth-token, Accept, X-Requested-With`");
             // 返回结果可以用于缓存的最长时间，单位是秒。-1表示禁用
-            response.addHeader("Access-Control-Max-Age", "3600");
+            response.addHeader("Access-Control-Max-Age", "36000");
+
             // 跨域预检请求，直接返回
             if (REQUEST_OPTIONS.equalsIgnoreCase(request.getMethod())) {
                 return;
